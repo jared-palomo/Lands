bf9bd6247a5534a9b5fb982a6fd1f10d16f42aa6
Jared <g.jared.palomo@gmail.com>
2020-06-02 17:20:54 -0500

    LoginViewModel

---
+++ lands/Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,21 @@
+﻿using Lands.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infrastructure
+{
+    class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
---
+++ lands/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Lands.ViewModels
+{
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsRemember { get; set; }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand { get; set; }
+        public ICommand RegisterCommand { get; set; }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemember = true;
+            this.IsRunning = false;
+        }
+        #endregion
+    }
+}
