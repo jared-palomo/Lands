53165ddbf951c5f203c5210a527156b34905d54d
Jared <g.jared.palomo@gmail.com>
2020-06-10 19:53:21 -0500

    New_Users1

---
+++ lands/Lands.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,13 @@
+﻿using Lands.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lands.Backend.Models
+{
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<Lands.Domain.User> Users { get; set; }
+    }
+}
--- lands/Lands.Domain/Response.cs
+++ lands/Lands.Domain/Response.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Lands.Models
+namespace Lands.Domain
 {
     public class Response
     {
